d83f3c06490ecd8fd1372b48eee30a251f90bdff
abvirk <se.abvirk@gmail.com>
2023-04-26 01:04:26 +0400

    First

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTimeSpan = TimeSpan.Zero;
+var periodTimeSpan = TimeSpan.FromSeconds(5);
+
+var timer = new System.Threading.Timer((e) =>
+{
+Console.WriteLine($"Running at {DateTime.Now.ToLocalTime()}");
+}, null, startTimeSpan, periodTimeSpan);
+Console.ReadLine();
+        }
+    }
+}
